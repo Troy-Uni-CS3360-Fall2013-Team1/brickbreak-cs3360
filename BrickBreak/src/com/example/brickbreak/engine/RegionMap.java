863cb3d93ed1380e6a7cf611b161d5d1fb84ceff
docplasma <thedocplasma@gmail.com>
2013-11-14 01:32:21 -0600

    Implemented clear() and split()

--- BrickBreak/src/com/example/brickbreak/engine/RegionMap.java
+++ BrickBreak/src/com/example/brickbreak/engine/RegionMap.java
@@ -10,12 +10,12 @@ public class RegionMap {
 	
 	private int mLevel;
 	private List mObjects;
-	private AABB mBounds;
+	private Rectangle mBounds;
 	private RegionMap[] mNodes;
 	
 	
 	
-	RegionMap(int level, AABB bounds) {
+	RegionMap(int level, Rectangle bounds) {
 		mLevel = level;
 		mBounds = bounds;
 		mObjects = new ArrayList();
@@ -26,16 +26,31 @@ public class RegionMap {
 	
 	//Clears the tree of all nodes/objects
 	public void clear() {
-		
+		mObjects.clear();
+		for (int i = 0; i < mObjects.size(); i++) {
+			if (mNodes[i] != null) {
+				mNodes[i].clear();
+				mNodes[i] = null;
+			}
+		}
 	}
 	
 	//Once a node reaches the maximum allowed children, it then splits the region into a 4 sub-regions
 	private void split() {
+		int mSubWidth = mBounds.getWidth() / 2;
+		int mSubHeight = mBounds.getHeight() / 2;
+		int mX = mBounds.getX();
+		int mY = mBounds.getY();
+		
+		mNodes[0] = new RegionMap(mLevel+1, new Rectangle(mX + mSubWidth, mY, mSubWidth, mSubHeight));
+		mNodes[1] = new RegionMap(mLevel+1, new Rectangle(mX, mY, mSubWidth, mSubHeight));
+		mNodes[2] = new RegionMap(mLevel+1, new Rectangle(mX, mY + mSubHeight, mSubWidth, mSubHeight));
+		mNodes[3] = new RegionMap(mLevel+1, new Rectangle(mX + mSubWidth, mY + mSubHeight, mSubWidth, mSubHeight));
 		
 	}
 	
 	//Inserts a new object into the map
-	public void insert(AABB aabb) {
+	public void insert(Brick brick) {
 		
 	}
 }
