602017e655e779ce8354ec68485eb6efc4389157
Justin <jcwilliams90@gmail.com>
2013-11-14 14:33:45 -0600

    Initial Commit

--- BrickBreak/src/com/example/brickbreak/engine/Brick.java
+++ BrickBreak/src/com/example/brickbreak/engine/Brick.java
@@ -1,5 +1,15 @@
 package com.example.brickbreak.engine;
 
-public class Brick {
+public class Brick extends Rectangle {
+	
+	public Brick(float pX, float pY, float pWidth, float pHeight) {
+        super(pX, pY, pWidth, pHeight);
+        // TODO Auto-generated constructor stub
+}
+
+protected void onManagedUpdate(final float pSecondsElapsed) {
+//                if(this.collidesWith(pOtherShape))
+
+}
 
 }
--- BrickBreak/src/com/example/brickbreak/engine/RegionMap.java
+++ BrickBreak/src/com/example/brickbreak/engine/RegionMap.java
@@ -1,5 +1,16 @@
 package com.example.brickbreak.engine;
 
 public class RegionMap {
+	private Brick[][] map;
+    public RegionMap(Brick[][] map) {
+            super();
+            this.map = map;
+    }
+
+
+    public Brick[][] getMap(){
+                    return this.map;
+    }
+    
 
 }
