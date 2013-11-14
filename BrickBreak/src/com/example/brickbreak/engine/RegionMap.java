513dad8ba41ad9a974e3cb373193fad40cd21820
docplasma <thedocplasma@gmail.com>
2013-11-14 02:09:15 -0600

    Implemented getIndex()

--- BrickBreak/src/com/example/brickbreak/engine/RegionMap.java
+++ BrickBreak/src/com/example/brickbreak/engine/RegionMap.java
@@ -1,11 +1,17 @@
 package com.example.brickbreak.engine;
-
+/*
+ * This class stores references to brick objects. It organizes them based on where they belong in a partitioned space.
+ * They are number 0, 1, 2, 3 counter clockwise.
+ * A list of objects can be obtained via the INSERTNAMEOFFUNCTIONTORETRIEVEOBJECTS for any given space
+ */
 import java.util.ArrayList;
 import java.util.List;
 
 public class RegionMap {
 
+	//This determines the maximum number of objects inside any region
 	private final int MAX_OBJECTS = 10;
+	//This determines the maximum number of sub-regions
 	private final int MAX_LEVELS = 5;
 	
 	private int mLevel;
@@ -24,7 +30,7 @@ public class RegionMap {
 		
 	}
 	
-	//Clears the tree of all nodes/objects
+	//Purges the tree of all nodes/objects
 	public void clear() {
 		mObjects.clear();
 		for (int i = 0; i < mObjects.size(); i++) {
@@ -49,8 +55,47 @@ public class RegionMap {
 		
 	}
 	
+	//Determines where (node) the object lives. -1 means that the object belongs to its parent node and not the child.
+	private int getIndex(AABB aabb) {
+		int index = -1;
+		//Finds the vertical midpoint
+		double mVerticalMidpoint = mBounds.getmX() + (mBounds.getWidth() / 2);
+		//Finds the horizontal midpoint
+		double mHorizontalMidpoint = mBounds.getY() + (mBounds.getHeight() / 2);
+		//Checks to see if the object is entirely in the top two quadrants
+		boolean mTopQuadrant = (aabb.getY() < mHorizontalMidpoint && aabb.getY() + aabb.getHeight() < mHorizontalMidpoint);
+		//Checks to see if the object is entirely in the bottom two quadrants
+		boolean mBottomQuadrant = (aabb.getY() > mHorizontalMidpoint);
+		
+		//Checks to see if the object is entirely in the left quadrant
+		if(aabb.getX() < mVerticalMidpoint && aabb.getX() + aabb.getWidth() < mVerticalMidpoint) {
+			if (mTopQuadrant) {
+				index = 1;
+			}
+			else if (mBottomQuadrant) {
+				index = 2;
+			}
+		}
+		//Checks to see if the object is entirely in the right quadrant
+		else if(aabb.getX() > mVerticalMidpoint) {
+			if (mTopQuadrant) {
+				index = 0;
+			}
+			else if (mBottomQuadrant) {
+				index = 3;
+			}
+		}
+		return index;
+	}
+	
 	//Inserts a new object into the map
 	public void insert(Brick brick) {
-		
+		//TODO Implement insert
 	}
+	
+	//Deletes an object from the RegionMap
+	public void delete(Brick brick) {
+		//TODO Implement delete
+	}
+	//TODO Investigate hash table for constant time access to any brick
 }
