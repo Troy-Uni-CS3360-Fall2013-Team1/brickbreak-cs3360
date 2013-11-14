63d3e8652ec6ab478e77849e063123108db1f4a0
docplasma <thedocplasma@gmail.com>
2013-11-14 01:43:00 -0600

    Now extends rectangle. Any collision detection specific code will now appear here.

--- BrickBreak/src/com/example/brickbreak/engine/AABB.java
+++ BrickBreak/src/com/example/brickbreak/engine/AABB.java
@@ -2,57 +2,18 @@ package com.example.brickbreak.engine;
 /*
  * This Class defines a box surrounding an object. Its x and y coordinates are aligned to the x and y axes of the current plane.
  */
-public class AABB {
+public class AABB extends Rectangle {
 
-	private int mX;
-	private int mY;
-	
-	private int mWidth;
-	private int mHeight;
-	
 	//-----Constructors
 	AABB() {
-		
+		super();
 	}
 	
 	//Creates a new AABB with the origin(x,y) the top left corner
 	AABB(int x, int y, int width, int height) {
-		mX = x;
-		mY = y;
-		mWidth = width;
-		mHeight = height;
+		super(x, y, width, height);
 	}
 
 	//-----Getters/Setters
-	public int getX() {
-		return mX;
-	}
-
-	public void setX(int x) {
-		mX = x;
-	}
-
-	public int getY() {
-		return mY;
-	}
-
-	public void setY(int y) {
-		mY = y;
-	}
-
-	public int getWidth() {
-		return mWidth;
-	}
-
-	public void setWidth(int width) {
-		mWidth = width;
-	}
-
-	public int getHeight() {
-		return mHeight;
-	}
-
-	public void setHeight(int height) {
-		mHeight = height;
-	}
+	
 }
