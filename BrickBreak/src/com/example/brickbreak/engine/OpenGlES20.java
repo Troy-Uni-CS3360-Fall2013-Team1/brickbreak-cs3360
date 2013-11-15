4eceebf03340c31e84bdc340b9b204bcd7030914
Justin <jcwilliams90@gmail.com>
2013-11-15 10:35:57 -0600

    Graphics engine

---
+++ BrickBreak/src/com/example/brickbreak/engine/OpenGlES20.java
@@ -0,0 +1,26 @@
+package com.example.brickbreak.engine;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+// mainly the graffics engine 
+
+
+public class OpenGlES20 extends Activity
+{
+	private GLSurfaceView mGLView;
+	
+	@Override
+	public void onCreate (Bundle savedInstanceState)
+	{
+		super.onCreate ( savedInstanceState);
+		
+		// Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+
+	}
+
+}
