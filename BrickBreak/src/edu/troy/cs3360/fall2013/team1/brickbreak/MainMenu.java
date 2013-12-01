4036915888a8cc7422ef9fb052412f8804b82c9a
docplasma <thedocplasma@gmail.com>
2013-12-01 16:22:29 -0600

    Added BG and associated files

--- BrickBreak/src/edu/troy/cs3360/fall2013/team1/brickbreak/MainMenu.java
+++ BrickBreak/src/edu/troy/cs3360/fall2013/team1/brickbreak/MainMenu.java
@@ -1,18 +1,23 @@
 package edu.troy.cs3360.fall2013.team1.brickbreak;
 
-import com.example.brickbreak.R;
-
-import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+
+import com.example.brickbreak.R;
 
 public class MainMenu extends Activity {
 	
 	
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
 		setContentView(R.layout.activity_main_menu);
 	}
 
