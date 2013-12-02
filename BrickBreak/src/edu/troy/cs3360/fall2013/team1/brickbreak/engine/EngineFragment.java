25e73e436561ac425e56aad77ef5fe6e5e5ef93e
docplasma <thedocplasma@gmail.com>
2013-12-02 11:24:47 -0600

    Created EngineFragment

---
+++ BrickBreak/src/edu/troy/cs3360/fall2013/team1/brickbreak/engine/EngineFragment.java
@@ -0,0 +1,25 @@
+package edu.troy.cs3360.fall2013.team1.brickbreak.engine;
+
+import com.example.brickbreak.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EngineFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.activity_score_fragment, container, false);
+		return view;
+		
+	}
+
+}
