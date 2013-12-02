da09a65c71644e5402509d023a004a6c7c7907ad
docplasma <thedocplasma@gmail.com>
2013-12-01 20:08:19 -0600

    Corrected Code
    Score Fragment XML:
    IDs were renamed to match convention.
    Removed unecessary TextViews/Buttons.
    Strings XML:
    Removed unecessary strings.
    Renamed strings to match convention.
    ScoreFragment Java:
    Removed Wrapping Class and changed inner class to extend Fragment.
    Changed Inner class to match file name.
    Removed currentScore/currentLive (not needed).
    Changed associated statements to use mScore/mLive.
    Removed unecessary TextView/Button.
    Deleted setContentView() (fragments do not call this).
    changed findViewById to getView().findViewById so they have a valid
    reference.

--- BrickBreak/src/edu/troy/cs3360/fall2013/team1/brickbreak/ScoreFragment.java
+++ BrickBreak/src/edu/troy/cs3360/fall2013/team1/brickbreak/ScoreFragment.java
@@ -2,61 +2,48 @@ package edu.troy.cs3360.fall2013.team1.brickbreak;
 
 
 import com.example.brickbreak.R;
+
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class ScoreFragment extends FragmentActivity
-{
-	
-	int mCurrentScore;
+public  class ScoreFragment extends Fragment {
+
 	int mScore=0000;
 	int mLives = 3;
-	int mCurrentLive;
 	TextView scoreView;
 	TextView livesView;
-	public  class brickbreak extends FragmentActivity {
-
+	Button menuButton;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) 
+	public void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_score_fragment);
-		}
-	    @Override
-	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	                             Bundle savedInstanceState) {
-	    	View view = inflater.inflate(R.layout.activity_score_fragment,
-	    	        container, false);
-	        // Inflate the layout for this fragment
-	    	setContentView(R.layout.activity_score_fragment);
-	    	scoreView = (TextView)findViewById(R.id.textViewScore);
-	    	scoreView.setText(mScore); // also can call a function return the score 
-	        
-	    	livesView = (TextView)findViewById(R.id.textViewLives);
-	    	livesView.setText(mLives); // also can call a function return the score 
-	    	
-	    	// the buttons for what will the menu hold  
-	    	Button btnMenu;
-	    	btnMenu=(Button)findViewById(R.id.buttonMenu);
-	    	btnMenu=(Button)findViewById(R.id.buttonMenu);
-	    	btnMenu=(Button)findViewById(R.id.buttonMenu);
-	    		    
-	    	      
-	    	// Set OnClick Listener on SignUp button 
-	    	btnMenu.setOnClickListener(new View.OnClickListener()
-	    	{
-	         
-	    	public void onClick(View v) 
-	         {
-	    	  // TODO Auto-generated method stub
-	    	 }
-	    	 });
-	        return view;
+	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    	View view = inflater.inflate(R.layout.activity_score_fragment, container, false);
+    	
+    	scoreView = (TextView) getView().findViewById(R.id.textViewScore);
+    	scoreView.setText(mScore); // also can call a function return the score 
+        
+    	livesView = (TextView) getView().findViewById(R.id.textViewLives);
+    	livesView.setText(mLives); // also can call a function return the score 
+    	
+    	// the buttons for what will the menu hold
+    	menuButton=(Button) getView().findViewById(R.id.buttonMenu);
+    	      
+    	// Set OnClick Listener on SignUp button 
+    	menuButton.setOnClickListener(new View.OnClickListener() {
+	    	public void onClick(View v) {
+	    	}
+    	});
+        
+    	return view;
 	}
     
 
@@ -64,16 +51,15 @@ public class ScoreFragment extends FragmentActivity
 	//Sample Methods to update score 
 	public void updateScore( int brickvalue)
 	 {
-		mCurrentScore += brickvalue;
-		scoreView.setText(Integer.toString(mCurrentScore));
+		mScore += brickvalue;
+		scoreView.setText(Integer.toString(mScore));
 	 }
 
 	//update lives
 	public void updatelives(int live)
 	{
-		mCurrentLive+=live;
-	    livesView.setText( Integer.toString(mCurrentLive));
+		mLives+=live;
+	    livesView.setText( Integer.toString(mLives));
      }
-
 }
 
