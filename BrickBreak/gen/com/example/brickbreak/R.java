de2bebb2c282d09dc83fcc5423d44734f2d53bbf
docplasma <thedocplasma@gmail.com>
2013-12-02 11:24:57 -0600

    Auto-gen

--- BrickBreak/gen/com/example/brickbreak/R.java
+++ BrickBreak/gen/com/example/brickbreak/R.java
@@ -39,14 +39,16 @@ public final class R {
         public static final int start_button=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_game_level_host=0x7f030000;
-        public static final int activity_main_menu=0x7f030001;
-        public static final int activity_score_fragment=0x7f030002;
+        public static final int activity_engine_fragment=0x7f030000;
+        public static final int activity_game_level_host=0x7f030001;
+        public static final int activity_main_menu=0x7f030002;
+        public static final int activity_score_fragment=0x7f030003;
     }
     public static final class menu {
-        public static final int game_level_host=0x7f070000;
-        public static final int main_menu=0x7f070001;
-        public static final int score=0x7f070002;
+        public static final int engine=0x7f070000;
+        public static final int game_level_host=0x7f070001;
+        public static final int main_menu=0x7f070002;
+        public static final int score=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -54,11 +56,13 @@ public final class R {
          */
         public static final int activity_main_menu_start_game=0x7f050006;
         public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f05000b;
         public static final int lives=0x7f050004;
         public static final int menu_button=0x7f050009;
         public static final int score=0x7f050003;
         public static final int start_lives=0x7f050008;
         public static final int start_score=0x7f050005;
+        public static final int title_activity_engine_fragment=0x7f05000a;
         public static final int title_activity_game_level_host=0x7f050007;
         public static final int title_activity_score=0x7f050002;
     }
