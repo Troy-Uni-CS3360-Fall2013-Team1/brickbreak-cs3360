163b6ee34c23181ff9d50cf2b55d6db73e660347
docplasma <thedocplasma@gmail.com>
2013-12-02 20:37:15 -0600

    Auto-gen

--- BrickBreak/gen/com/example/brickbreak/R.java
+++ BrickBreak/gen/com/example/brickbreak/R.java
@@ -10,6 +10,10 @@ package com.example.brickbreak;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f050000;
+        public static final int colors=0x7f050001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,8 +21,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -26,17 +30,17 @@ public final class R {
         public static final int overflow=0x7f020002;
     }
     public static final class id {
-        public static final int MasterViewGroup=0x7f080002;
-        public static final int action_settings=0x7f08000a;
-        public static final int centerPoint=0x7f080007;
-        public static final int current_lives_text_view=0x7f080005;
-        public static final int current_score_text_view=0x7f080008;
-        public static final int engine_fragment=0x7f080001;
-        public static final int lives_text_view=0x7f080004;
-        public static final int main_menu_button=0x7f080009;
-        public static final int score_fragment=0x7f080000;
-        public static final int score_text_view=0x7f080006;
-        public static final int start_button=0x7f080003;
+        public static final int MasterViewGroup=0x7f0a0002;
+        public static final int action_settings=0x7f0a000a;
+        public static final int centerPoint=0x7f0a0007;
+        public static final int current_lives_text_view=0x7f0a0005;
+        public static final int current_score_text_view=0x7f0a0008;
+        public static final int engine_fragment=0x7f0a0001;
+        public static final int lives_text_view=0x7f0a0004;
+        public static final int main_menu_button=0x7f0a0009;
+        public static final int score_fragment=0x7f0a0000;
+        public static final int score_text_view=0x7f0a0006;
+        public static final int start_button=0x7f0a0003;
     }
     public static final class layout {
         public static final int activity_engine_fragment=0x7f030000;
@@ -45,26 +49,29 @@ public final class R {
         public static final int activity_score_fragment=0x7f030003;
     }
     public static final class menu {
-        public static final int engine=0x7f070000;
-        public static final int game_level_host=0x7f070001;
-        public static final int main_menu=0x7f070002;
-        public static final int score=0x7f070003;
+        public static final int engine=0x7f090000;
+        public static final int game_level_host=0x7f090001;
+        public static final int main_menu=0x7f090002;
+        public static final int score=0x7f090003;
+    }
+    public static final class raw {
+        public static final int level_1=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_settings=0x7f070001;
         /**  activity_main_menu 
          */
-        public static final int activity_main_menu_start_game=0x7f050006;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f05000b;
-        public static final int lives=0x7f050004;
-        public static final int menu_button=0x7f050009;
-        public static final int score=0x7f050003;
-        public static final int start_lives=0x7f050008;
-        public static final int start_score=0x7f050005;
-        public static final int title_activity_engine_fragment=0x7f05000a;
-        public static final int title_activity_game_level_host=0x7f050007;
-        public static final int title_activity_score=0x7f050002;
+        public static final int activity_main_menu_start_game=0x7f070006;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f07000b;
+        public static final int lives=0x7f070004;
+        public static final int menu_button=0x7f070009;
+        public static final int score=0x7f070003;
+        public static final int start_lives=0x7f070008;
+        public static final int start_score=0x7f070005;
+        public static final int title_activity_engine_fragment=0x7f07000a;
+        public static final int title_activity_game_level_host=0x7f070007;
+        public static final int title_activity_score=0x7f070002;
     }
     public static final class style {
         /** 
@@ -88,9 +95,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
     }
 }
