79c45c5b1ffc6cc8d1ad61a97e5117ecaa664366
docplasma <thedocplasma@gmail.com>
2013-12-03 00:05:33 -0600

    Adding BugFix For Score
    Changed Cast of Button to ImageButton.
    Changed arguments to setText to strings instead of ints.

--- BrickBreak/src/edu/troy/cs3360/fall2013/team1/brickbreak/ScoreFragment.java
+++ BrickBreak/src/edu/troy/cs3360/fall2013/team1/brickbreak/ScoreFragment.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.brickbreak.R;
 
 public  class ScoreFragment extends Fragment {
 
-	int mScore=0000;
-	int mLives = 3;
-	//TextView scoreView;
-	//TextView livesView;
-	//Button menuButton;
+	int mScore;
+	int mLives;
+	TextView scoreView;
+	TextView livesView;
+	ImageButton overflowButton;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -26,20 +26,23 @@ public  class ScoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     	View view = inflater.inflate(R.layout.activity_score_fragment, container, false);
     	
-    	//scoreView = (TextView) getView().findViewById(R.id.current_score_text_view);
-    	//scoreView.setText(mScore); // also can call a function return the score 
+    	mScore = 0000;
+    	mLives = 3;
+    	
+    	scoreView = (TextView) view.findViewById(R.id.current_score_text_view);
+    	scoreView.setText(Integer.toString(mScore)); // also can call a function return the score 
         
-    	//livesView = (TextView) getView().findViewById(R.id.current_lives_text_view);
-    	//livesView.setText(mLives); // also can call a function return the score 
+    	livesView = (TextView) view.findViewById(R.id.current_lives_text_view);
+    	livesView.setText(Integer.toString(mLives)); // also can call a function return the score 
     	
     	// the buttons for what will the menu hold
-    	//menuButton=(Button) getView().findViewById(R.id.main_menu_button);
+    	overflowButton=(ImageButton) view.findViewById(R.id.overflow_button);
     	      
     	// Set OnClick Listener on SignUp button 
-    	/*menuButton.setOnClickListener(new View.OnClickListener() {
+    	overflowButton.setOnClickListener(new View.OnClickListener() {
 	    	public void onClick(View v) {
 	    	}
-    	});*/
+    	});
         
     	return view;
 	}
@@ -50,14 +53,14 @@ public  class ScoreFragment extends Fragment {
 	public void updateScore(int brickvalue)
 	 {
 		mScore += brickvalue;
-		//scoreView.setText(Integer.toString(mScore));
+		scoreView.setText(Integer.toString(mScore));
 	 }
 
 	//update lives
 	public void updateLives(int live)
 	{
 		mLives+=live;
-	    //livesView.setText( Integer.toString(mLives));
+	    livesView.setText(Integer.toString(mLives));
      }
 }
 
