60c9211198434b606fbbbc80a580661de517a0fe
Justin <jcwilliams90@gmail.com>
2013-12-03 01:06:44 -0600

    Adding
    Adding XML codes to Drawable to be able to drag and use shapes.

--- BrickBreak/gen/com/example/brickbreak/R.java
+++ BrickBreak/gen/com/example/brickbreak/R.java
@@ -25,9 +25,21 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int main_menu_bg=0x7f020001;
-        public static final int overflow=0x7f020002;
+        public static final int blue=0x7f020000;
+        public static final int cyan=0x7f020001;
+        public static final int grey=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int light_blue=0x7f020004;
+        public static final int lime=0x7f020005;
+        public static final int magenta=0x7f020006;
+        public static final int main_menu_bg=0x7f020007;
+        public static final int orange=0x7f020008;
+        public static final int overflow=0x7f020009;
+        public static final int purple=0x7f02000a;
+        public static final int red=0x7f02000b;
+        public static final int shape=0x7f02000c;
+        public static final int shape_droptarget=0x7f02000d;
+        public static final int yellow=0x7f02000e;
     }
     public static final class id {
         public static final int MasterViewGroup=0x7f0a0002;
